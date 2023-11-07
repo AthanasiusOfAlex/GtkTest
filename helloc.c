347e49ac501f0b3af14ad0afee7493fcf584d2ea
AthanasiusOfAlex <lmelahn@legionaries.org>
2023-11-07 09:27:52 +0100

    Saved again

--- helloc.c
+++ helloc.c
@@ -5,10 +5,27 @@ activate (GtkApplication* app,
           gpointer        user_data)
 {
   GtkWidget *window;
+  GtkWidget *menu_bar;
+  GMenu *menu_model;
 
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "Window");
   gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+  // Create a menu bar widget
+  menu_bar = gtk_popover_menu_bar_new ();
+
+  // Create a menu model with a File menu and a Quit menu item
+  menu_model = g_menu_new ();
+  g_menu_append (menu_model, "File", "file");
+  g_menu_append_submenu (menu_model, "Quit", g_menu_new ());
+
+  // Set the menu model of the menu bar
+  gtk_popover_menu_bar_set_menu_model (GTK_POPOVER_MENU_BAR (menu_bar), G_MENU_MODEL (menu_model));
+
+  // Set the menu bar as the title bar of the window
+  gtk_window_set_titlebar (GTK_WINDOW (window), menu_bar);
+
   gtk_window_present (GTK_WINDOW (window));
 }
 
